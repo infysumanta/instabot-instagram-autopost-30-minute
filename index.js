1248a3c95dae6d267d5e8dd57edc104367c1a3fd
Sumanta Kabiraj <sumantablog@gmail.com>
2023-06-12 01:48:03 +0530

    updated

--- index.js
+++ index.js
@@ -1,33 +1,67 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
-const fs = require("fs");
+const fs = require('fs');
+const nodemailer = require('nodemailer');
+const { IgApiClient } = require('instagram-private-api');
+const { get } = require('request-promise');
+const CronJob = require('cron').CronJob;
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-const { IgApiClient } = require("instagram-private-api");
-const { get } = require("request-promise");
-const CronJob = require("cron").CronJob;
+let transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.GOOGLE_USERNAME, // generated ethereal user
+    pass: process.env.GOOGLE_PASSWORD, // generated ethereal password
+  },
+});
 
 const ig = new IgApiClient();
 
 const login = async () => {
   ig.state.generateDevice(process.env.IG_USERNAME);
   await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
-  console.log("Logged in");
+  console.log('Logged in');
 };
 
-const postToInsta = async () => {
-  console.log(
-    "Posting to Instagram at",
-    new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" })
-  );
+const sendMail = async () => {
+  let info = await transporter.sendMail({
+    from: `"Instabot 👻" <${process.env.GOOGLE_USERNAME}>`, // sender address
+    to: process.env.GMAIL_SENDER, // list of receivers
+    subject: 'Instabot  Post Posted✔', // Subject line
+    text: `Posted to Instagram at ${new Date().toLocaleString('en-US', {
+      timeZone: 'Asia/Kolkata',
+    })}`,
+  });
 
+  console.log('Message sent: %s', info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+};
+
+const sendMailNotSend = async () => {
+  let info = await transporter.sendMail({
+    from: `"Instabot 👻 (Not)" <${process.env.GOOGLE_USERNAME}>`, // sender address
+    to: process.env.GMAIL_SENDER, // list of receivers
+    subject: 'Instabot  Post Posted✔', // Subject line
+    text: `Not Posted to Instagram at ${new Date().toLocaleString('en-US', {
+      timeZone: 'Asia/Kolkata',
+    })}`,
+  });
+
+  console.log('Message sent: %s', info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+};
+
+const sendPost = async () => {
   const imageBuffer = await get({
-    url: "https://picsum.photos/200/300",
+    url: 'https://picsum.photos/200/300',
     encoding: null,
   });
   // get from file from image.jpg file
@@ -35,21 +69,39 @@ const postToInsta = async () => {
 
   await ig.publish.photo({
     file: imageBuffer,
-    caption: "Really nice photo from the internet!",
+    caption: 'Really nice photo from the internet!',
   });
-  console.log(
-    "Posted to Instagram at",
-    new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" })
-  );
 };
 
-const cronInsta = new CronJob("*/30 * * * *", async () => {
+const postToInsta = async () => {
+  const ifPost = Math.floor(Math.random() * 2);
+  console.log('ifPost', ifPost);
+  if (ifPost === 1) {
+    const postTimer = 0; //Math.floor(Math.random() * 61);
+    console.log('postTimer', postTimer);
+    setTimeout(async () => {
+      console.log(
+        'Posting to Instagram at',
+        new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }),
+      );
+      await sendPost();
+      await sendMail();
+      console.log(
+        'Posted to Instagram at',
+        new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }),
+      );
+    }, postTimer * 1000);
+  } else {
+    await sendMailNotSend();
+  }
+};
+
+const cronInsta = new CronJob('*/60 * * * *', async () => {
   postToInsta();
 });
 
 login()
   .then(() => {
-    postToInsta();
     cronInsta.start();
   })
   .catch((err) => {
